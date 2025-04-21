a870186b79fe208bc241c2c3c539ff6ef76134a7
Damian <damiansuaz@gmail.com>
2025-04-21 09:33:13 -0400

    Create tasktracker.py

---
+++ tasktracker.py
@@ -0,0 +1,9 @@
+from argsparse import ArgumentParser
+from datetime import datetime
+from tabulate import tabulate
+import json, os, sys
+
+if __name__ == '__main__':
+    # get import from local json
+    # 
+    exit 0
